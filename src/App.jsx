1fb866cf7b5e8233fce9709db0bf3736f1c8fbde
Marcio Coutinho <marciohbc@gmail.com>
2023-02-02 10:33:44 -0300

    Config básica

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+function App() {
+    // Retorna uma expressão JSX
+    return <h1>Hello World!!!</h1>
+}
+
+export default App
---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+// Importe as bibliotecas básicas do React
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App"
+
+const root = document.getElementById('root');
+
+const react = ReactDOM.createRoot(root);
+
+react.render(<App />)
